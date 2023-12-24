5cd3766fb00ed483043cb1c921fdbe34908ba9eb
diovanedm <diovane.dm@gmail.com>
2023-12-24 18:31:45 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
+export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Diovane Maia Soares",
+    email: "diovane.gmail.com",
+  },
+});
