30d0162bd433de74878c6035c8da75a686e5b60e
diovanedm <diovane.dm@gmail.com>
2024-01-20 18:10:43 -0300

    organize packages

--- src/http/controllers/users/profile.spec.ts
+++ src/http/controllers/users/profile.spec.ts
@@ -2,6 +2,7 @@ import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import request from "supertest";
 import { app } from "@/app";
 import supertest from "supertest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
 
 describe("Profile (e2e)", async () => {
   beforeAll(async () => {
@@ -13,25 +14,13 @@ describe("Profile (e2e)", async () => {
   });
 
   it("should be able to authenticate", async () => {
-    await request(app.server).post("/users").send({
-      name: "Diovane",
-      email: "diovane.maia@gmail.com",
-      password: "123456",
-    });
-
-    const authResponse = await request(app.server).post("/sessions").send({
-      email: "diovane.maia@gmail.com",
-      password: "123456",
-    });
-
-    const { token } = authResponse.body;
-
+    const { token } = await createAndAuthenticateUser(app);
     const profileResponse = await request(app.server)
       .get("/me")
       .set("Authorization", `Bearer ${token}`)
       .send();
 
-    expect(authResponse.status).toEqual(200);
+    expect(profileResponse.statusCode).toEqual(200);
     expect(profileResponse.body).toEqual(
       expect.objectContaining({
         email: "diovane.maia@gmail.com",
